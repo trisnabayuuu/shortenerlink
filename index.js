21b2f7f3c57e5bb878e51417cf73243216d0ef5a
Trisna Bayu <btrisna90@gmail.com>
2023-01-15 23:44:11 +0700

    cors

--- index.js
+++ index.js
@@ -20,8 +20,14 @@
 //     console.log(`Example app listening on port ${port}`)
 // })
 
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
+const cors = require('cors');
+const port = 5000;
+
+app.use(cors({
+    origin: "http://127.0.0.1:5173",
+}))
 
 // Database config
 const connection = require('./config/db.config')
@@ -31,10 +37,10 @@ connection.on('error', () => console.log('Error'))
 // Routes Config
 app.use(express.json({
     extended: false
-})) //parse incoming request body in JSON format.
+})) 
 app.use('/', require('./routes/redirect'))
 app.use('/api/url', require('./routes/url'))
 
-//Listen for incoming requests
-const PORT = process.env.PORT || 5000
-app.listen(PORT, console.log(`server started, listening PORT ${PORT}`))
+app.listen(port, () => {
+    console.log(`cli-nodejs-api listening at http://127.0.0.1:${port}`)
+  })
